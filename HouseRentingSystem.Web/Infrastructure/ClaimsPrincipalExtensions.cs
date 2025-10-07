ad819a11c17686481261c8e37a79643f2e314650
Nikolay Angelov <n.angelov90@abv.bg>
2025-10-07 13:17:03 +0300

    Init repo

---
+++ HouseRentingSystem.Web/Infrastructure/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+using static HouseRentingSystem.Web.Areas.Admin.AdminConstants;
+
+namespace HouseRentingSystem.Web.Infrastructure
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string Id(this ClaimsPrincipal user)
+        {
+            var claim = user.FindFirst(ClaimTypes.NameIdentifier);
+            return claim?.Value ?? string.Empty;
+        }
+
+        public static bool IsAdmin(this ClaimsPrincipal user)
+           => user.IsInRole(AdminRoleName);
+    }
+}
